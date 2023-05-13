ed3ea914e46754754049ba2af7c957bd7b6aba9a
Anshuman301 <anshumanprsd@gmail.com>
2023-05-13 16:24:47 +0530

    addede mediastream

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,46 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.scss";
-const peer = new window.Peer("afgs236-sgshg-2627a", {
+const peer = new window.Peer("cbnbfj34-djhd-djdj34", {
   host: "localhost",
   port: 9000,
   path: "/chat",
-  debug: 0,
+  debug: 3,
 });
 
 peer.on("open", (id) => {
   console.log("The generated id is:", id);
 });
 function App() {
-  const refConn = useRef(null);
   const inputRef = useRef(null);
-  const handleClick = () => {
-    const dataConnection = peer.connect("cbnbfj34-djhd-djdj34", {
-      serialization: "utf-8",
-    });
-    refConn.current = dataConnection;
-  };
-  peer.on("connection", function (conn) {
-    console.log(conn);
-    refConn.current = conn;
-  });
-
   useEffect(() => {
-    const { current } = refConn;
-    if (current) {
-      current.on("data", function (data) {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: true,
+        video: true,
+      })
+      .then((value) => {
+        console.log(value);
+      })
+      .catch((reason) => console.log(reason));
+  }, []);
+  peer.on("connection", function (conn) {
+    conn.on("open", function () {
+      // Receive messages
+      console.log(conn);
+      conn.on("data", function (data) {
         console.log("Received", data);
       });
-    }
-    return () => {
-      if (current) current.off("data");
-    };
-  }, [refConn.current]);
+    });
+  });
   const handleSend = () => {
-    const { current: dataConnection } = refConn;
-    if (dataConnection)
-      dataConnection.on("open", function () {
-        console.log("OPEN");
-        // Send messages
-        dataConnection.send("Hello!");
-      });
+    const dc = peer.connect("afgs236-sgshg-2627a");
+    dc.on("open", function () {
+      dc.send(inputRef.current.value);
+    });
   };
   return (
     <div>
-      <button onClick={handleClick}>Connect</button>
+      {/* <button onClick={handleClick}>Connect</button> */}
       <input name="inputText" ref={inputRef} />
       <button onClick={handleSend}>Send</button>
     </div>
